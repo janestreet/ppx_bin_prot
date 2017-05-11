aad12d18c17a5ed84112cb3c36acf3e9bbdc5ac1
Thomas Refis <thomas.refis@gmail.com>
2017-05-11 16:25:49 +0100

    v0.9.114.44+120

--- expect_tests/allocation_tests.ml
+++ expect_tests/allocation_tests.ml
@@ -11,23 +11,17 @@ let%expect_test "No allocation when writing variants" =
   end in
   let writer = Test.bin_writer_t in
   let buf = Bigstring.create 1024 in
-  let _ : int = show_allocation (fun () -> writer.write buf ~pos:0 No_arg) in
+  let _ : int =
+    require_no_allocation [%here] (fun () -> writer.write buf ~pos:0 No_arg) in
   let one_arg = Test.One_arg "foo" in
-  let () = [%expect {|
-    (allocated
-      (minor_words 0)
-      (major_words 0)) |}] in
-  let _ : int = show_allocation (fun () -> writer.write buf ~pos:0 one_arg) in
-  let () = [%expect {|
-    (allocated
-      (minor_words 0)
-      (major_words 0)) |}] in
+  let () = [%expect {| |}] in
+  let _ : int =
+    require_no_allocation [%here] (fun () -> writer.write buf ~pos:0 one_arg) in
+  let () = [%expect {| |}] in
   let record_arg = Test.Record_arg { num = 5 } in
-  let _ : int = show_allocation (fun () -> writer.write buf ~pos:0 record_arg) in
-  [%expect {|
-    (allocated
-      (minor_words 0)
-      (major_words 0)) |}];
+  let _ : int =
+    require_no_allocation [%here] (fun () -> writer.write buf ~pos:0 record_arg) in
+  [%expect {| |}];
 ;;
 
 let%expect_test "No allocation when writing polymorphic variant" =
@@ -39,15 +33,11 @@ let%expect_test "No allocation when writing polymorphic variant" =
   end in
   let writer = Test.bin_writer_t in
   let buf = Bigstring.create 1024 in
-  let _ : int = show_allocation (fun () -> writer.write buf ~pos:0 `No_arg) in
+  let _ : int =
+    require_no_allocation [%here] (fun () -> writer.write buf ~pos:0 `No_arg) in
   let one_arg = `One_arg "foo" in
-  let () = [%expect {|
-    (allocated
-      (minor_words 0)
-      (major_words 0)) |}] in
-  let _ : int = show_allocation (fun () -> writer.write buf ~pos:0 one_arg) in
-  [%expect {|
-    (allocated
-      (minor_words 0)
-      (major_words 0)) |}]
+  let () = [%expect {| |}] in
+  let _ : int =
+    require_no_allocation [%here] (fun () -> writer.write buf ~pos:0 one_arg) in
+  [%expect {| |}]
 ;;
